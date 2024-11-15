6d825a78d06927ec075b29851cbce4b525d2eafa
Lovie-Tonimba <jechonialovie@gmail.com>
2024-11-15 09:52:18 +0700

    experiment 1

---
+++ ArrayNumbers244107060101.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArrayNumbers244107060101 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [] num = new int [4];
+        num [0] = 5;
+        num [1] = 12;
+        num [2] = 7;
+        num [3] = 20;
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+    }
+}
